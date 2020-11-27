3e6b1cea978a23aa26a8d32496e252c95c713a3f
Bruno Martins <bgcngm@gmail.com>
2020-11-27 11:54:28 +0000

    KeyboardSettings: Add a RemotePreference for device-specific panel
    Allow devices to provide advanced keyboard settings.
    
    Unfortunatelly this can't be done with existing IA settings,
    so use the RemotePreference API to allow the external app
    to update the view. The action for this is:
    
         "org.lineageos.settings.device.ADVANCED_KEYBOARD_SETTINGS"
    
    Change-Id: I1c46d3568c4d5bfc78e1cbae033071c7641fdf29
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>
    Signed-off-by: Nalinish Ranjan <nalinishranjan05@gmail.com>

--- src/com/android/settings/inputmethod/PhysicalKeyboardFragment.java
+++ src/com/android/settings/inputmethod/PhysicalKeyboardFragment.java
@@ -71,6 +71,7 @@ public final class PhysicalKeyboardFragment extends DashboardFragment
 
     private static final String KEYBOARD_OPTIONS_CATEGORY = "keyboard_options_category";
     private static final String KEYBOARD_A11Y_CATEGORY = "keyboard_a11y_category";
+    private static final String KEYBOARD_EXTRAS_CATEGORY = "keyboard_extras_category";
     private static final String ACCESSIBILITY_BOUNCE_KEYS = "accessibility_bounce_keys";
     private static final String ACCESSIBILITY_SLOW_KEYS = "accessibility_slow_keys";
     private static final String ACCESSIBILITY_STICKY_KEYS = "accessibility_sticky_keys";
@@ -105,6 +106,8 @@ public final class PhysicalKeyboardFragment extends DashboardFragment
     @Nullable
     private PreferenceCategory mKeyboardA11yCategory = null;
     @Nullable
+    private PreferenceCategory mKeyboardExtrasCategory;
+    @NonNull
     private TwoStatePreference mAccessibilityBounceKeys = null;
     @Nullable
     private TwoStatePreference mAccessibilitySlowKeys = null;
@@ -144,6 +147,8 @@ public final class PhysicalKeyboardFragment extends DashboardFragment
         mImm = Preconditions.checkNotNull(activity.getSystemService(InputMethodManager.class));
         mKeyboardAssistanceCategory = Preconditions.checkNotNull(
                 findPreference(KEYBOARD_OPTIONS_CATEGORY));
+        mKeyboardExtrasCategory = Preconditions.checkNotNull(
+                (PreferenceCategory) findPreference(KEYBOARD_EXTRAS_CATEGORY));
 
         mKeyboardA11yCategory = Objects.requireNonNull(findPreference(KEYBOARD_A11Y_CATEGORY));
         mAccessibilityBounceKeys = Objects.requireNonNull(
@@ -346,6 +351,8 @@ public final class PhysicalKeyboardFragment extends DashboardFragment
         if (mSupportsFirmwareUpdate) {
             mFeatureProvider.registerKeyboardInformationCategory(preferenceScreen);
         }
+        mKeyboardExtrasCategory.setOrder(99);
+        preferenceScreen.addPreference(mKeyboardExtrasCategory);
 
         if (InputSettings.isAccessibilityBounceKeysFeatureEnabled()
                 || InputSettings.isAccessibilityStickyKeysFeatureEnabled()
