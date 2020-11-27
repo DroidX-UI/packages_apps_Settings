eeab8edf43eab7896dbd7e91f354540651e8c760
Bruno Martins <bgcngm@gmail.com>
2020-11-27 11:54:28 +0000

    KeyboardSettings: Add a RemotePreference for device-specific panel
    Allow devices to provide advanced keyboard settings.
    
    Unfortunatelly this can't be done with existing IA settings,
    so use the RemotePreference API to allow the external app
    to update the view. The action for this is:
    
         "org.lineageos.settings.device.ADVANCED_KEYBOARD_SETTINGS"
    
    Change-Id: I1c46d3568c4d5bfc78e1cbae033071c7641fdf29
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- src/com/android/settings/inputmethod/PhysicalKeyboardFragment.java
+++ src/com/android/settings/inputmethod/PhysicalKeyboardFragment.java
@@ -70,6 +70,7 @@ public final class PhysicalKeyboardFragment extends SettingsPreferenceFragment
 
     private static final String KEYBOARD_OPTIONS_CATEGORY = "keyboard_options_category";
     private static final String KEYBOARD_A11Y_CATEGORY = "keyboard_a11y_category";
+    private static final String KEYBOARD_EXTRAS_CATEGORY = "keyboard_extras_category";
     private static final String SHOW_VIRTUAL_KEYBOARD_SWITCH = "show_virtual_keyboard_switch";
     private static final String ACCESSIBILITY_BOUNCE_KEYS = "accessibility_bounce_keys";
     private static final String ACCESSIBILITY_SLOW_KEYS = "accessibility_slow_keys";
@@ -100,6 +101,8 @@ public final class PhysicalKeyboardFragment extends SettingsPreferenceFragment
     @Nullable
     private PreferenceCategory mKeyboardA11yCategory = null;
     @Nullable
+    private PreferenceCategory mKeyboardExtrasCategory;
+    @NonNull
     private TwoStatePreference mShowVirtualKeyboardSwitch = null;
     @Nullable
     private TwoStatePreference mAccessibilityBounceKeys = null;
@@ -130,6 +133,8 @@ public final class PhysicalKeyboardFragment extends SettingsPreferenceFragment
         mImm = Preconditions.checkNotNull(activity.getSystemService(InputMethodManager.class));
         mKeyboardAssistanceCategory = Preconditions.checkNotNull(
                 findPreference(KEYBOARD_OPTIONS_CATEGORY));
+        mKeyboardExtrasCategory = Preconditions.checkNotNull(
+                (PreferenceCategory) findPreference(KEYBOARD_EXTRAS_CATEGORY));
         mShowVirtualKeyboardSwitch = Objects.requireNonNull(
                 mKeyboardAssistanceCategory.findPreference(SHOW_VIRTUAL_KEYBOARD_SWITCH));
 
@@ -325,6 +330,8 @@ public final class PhysicalKeyboardFragment extends SettingsPreferenceFragment
         if (mSupportsFirmwareUpdate) {
             mFeatureProvider.addFirmwareUpdateCategory(getPrefContext(), preferenceScreen);
         }
+        mKeyboardExtrasCategory.setOrder(99);
+        preferenceScreen.addPreference(mKeyboardExtrasCategory);
         updateShowVirtualKeyboardSwitch();
 
         if (InputSettings.isAccessibilityBounceKeysFeatureEnabled()
