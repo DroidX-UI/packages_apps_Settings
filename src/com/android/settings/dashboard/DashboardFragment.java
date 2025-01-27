e2727d645fb132e05f5326c46d77e9a85b20c487
AnierinB <anierin@evolution-x.org>
2025-01-27 20:36:56 +0000

    Settings: Set key order on addition
    When the activity is created, injected preferences were not setting their order. This could potentially lead to two bottom preferences
    in a given category on rebind. Ensure the order is set during key addition to prevent such issues.
    
    Follow up to change: I1dc0148c5595f20daf026970d6514e63776b0499
    
    Change-Id: I9df0bafbc2d2eddde94f07104d803fdc46adec8c
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/com/android/settings/dashboard/DashboardFragment.java
+++ src/com/android/settings/dashboard/DashboardFragment.java
@@ -648,6 +648,10 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                 observers = mDashboardFeatureProvider.bindPreferenceToTileAndGetObservers(
                         getActivity(), this, forceRoundedIcons, pref, tile, key,
                         mPlaceholderPreferenceController.getOrder());
+                // Order the prefs within their respective category
+                if (KEY_ORDER.containsKey(key)) {
+                    pref.setOrder(KEY_ORDER.get(key));
+                }
                 if (Flags.dynamicInjectionCategory()) {
                     if (tile.hasGroupKey()) {
                         Preference group = screen.findPreference(tile.getGroupKey());
